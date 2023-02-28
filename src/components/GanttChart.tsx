87e6fa950e7e305d817a481b074528e4e60528ec
tmilewski <me@tm.codes>
2023-02-28 12:09:25 -0500

    chore: save interview result

--- src/components/GanttChart.tsx
+++ src/components/GanttChart.tsx
@@ -1,19 +1,51 @@
-import React from "react";
-// import { getColorFromString } from "@parssa/universal-ui";
+import React, { useMemo } from "react"
+import { getColorFromString } from "@parssa/universal-ui"
 
-type DivProps = React.HTMLAttributes<HTMLDivElement>;
+type DivProps = React.HTMLAttributes<HTMLDivElement>
+
+// export const GanttItemContainer = ({ item }: {item: Item}) => {
+
+// }
+
+const scale = 0.0000002
 
 // You can use `getColorFromString` to get the colors for the Gantt Chart rows
 export const GanttChart = ({
   items,
   ...props
 }: DivProps & {
-  items: Item[];
-  }) => {
+  items: Item[]
+}) => {
+  const sortedItems = useMemo(() => {
+    return items
+      .sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime())
+      .map((item) => ({
+        ...item,
+        startDate: new Date(item.start).getTime(),
+        endDate: new Date(item.end).getTime(),
+      }))
+  }, [items])
+
+  // const totalTime = globalEndDate - globalStartDate
+
   return (
-    <div {...props}>
+    <div className="flex flex-col gap-4 text-sm" {...props}>
+      {sortedItems.map((item) => (
+        <div className="flex gap-4">
+          <div
+            className="h-6 rounded"
+            style={{
+              backgroundColor: getColorFromString(item.name),
+              marginLeft: (item.startDate - sortedItems[0].startDate) * scale,
+              width: (item.startDate - item.endDate) * scale,
+            }}
+          ></div>
+          <p>{item.name}</p>
+        </div>
+      ))}
+
       {/* Feel free to remove this */}
-      <pre>{JSON.stringify(items, null, 2)}</pre>
+      <pre>{JSON.stringify(sortedItems, null, 2)}</pre>
     </div>
-  );
-};
+  )
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,77 +1,77 @@
 export const items = [
   {
-    start: "2022-10-01",
-    end: "2022-10-05",
-    name: "Autocomplete MVP"
-  },
-  {
     start: "2022-10-02",
     end: "2022-10-03",
-    name: "Autocomplete MVP (1)"
+    name: "Autocomplete MVP (1)",
+  },
+  {
+    start: "2022-10-01",
+    end: "2022-10-05",
+    name: "Autocomplete MVP",
   },
   {
     start: "2022-10-02",
     end: "2022-10-08",
-    name: "Dotfiles Editor"
+    name: "Dotfiles Editor",
   },
   {
     start: "2022-10-03",
     end: "2022-10-04",
-    name: "Brainstorming"
+    name: "Brainstorming",
   },
   {
     start: "2022-10-03",
     end: "2022-10-05",
-    name: "Series A Funding"
+    name: "Series A Funding",
   },
   {
     start: "2022-10-04",
     end: "2022-10-08",
-    name: "Product Design"
+    name: "Product Design",
   },
   {
     start: "2022-10-06",
     end: "2022-10-13",
-    name: "Company Offsite"
+    name: "Company Offsite",
   },
   {
     start: "2022-10-08",
     end: "2022-10-19",
-    name: "Workflows MVP"
+    name: "Workflows MVP",
   },
   {
     start: "2022-10-09",
     end: "2022-10-11",
-    name: "Project Kickoff"
+    name: "Project Kickoff",
   },
   {
     start: "2022-10-12",
     end: "2022-11-04",
-    name: "Beta Testing"
+    name: "Beta Testing",
   },
   {
     start: "2022-10-28",
     end: "2022-11-12",
-    name: "Access Launch"
+    name: "Access Launch",
   },
   {
     start: "2022-11-01",
     end: "2022-11-10",
-    name: "Enterprise Launch"
+    name: "Enterprise Launch",
   },
   {
     start: "2022-11-02",
     end: "2022-11-15",
-    name: "Billing Infra"
+    name: "Billing Infra",
   },
   {
     start: "2022-11-04",
     end: "2022-11-12",
-    name: "New Website"
+    name: "New Website",
   },
   {
     start: "2022-11-06",
     end: "2022-11-09",
-    name: "Hiring Sprint"
-  }
-];
+    name: "Hiring Sprint",
+  },
+]
